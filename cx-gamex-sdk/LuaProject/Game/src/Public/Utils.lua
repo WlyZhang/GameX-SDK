b4e5f32eb397dc9d0b19a64ae3be53b1b83a1fbe
Wly.Zhang <zwlygame@yeah.net>
2023-10-15 16:07:50 +0800

    change utils.lua

--- cx-gamex-sdk/LuaProject/Game/src/Public/Utils.lua
+++ cx-gamex-sdk/LuaProject/Game/src/Public/Utils.lua
@@ -36,23 +36,23 @@ end
 --分割字符串
 function Utils:split(input, delimiter)
     
-    local arr = CS.GameUtils.Split(input,delimiter)
+    local arr = CS.API.Split(input,delimiter)
     return arr
 end
 
 --判断字符串是否为空
 function Utils:isNull(data)
-    return CS.GameUtils.IsNull(data)
+    return CS.API.IsNull(data)
 end
 
 --判断是否包含字符串
 function Utils:isContains(data,str)
-    return CS.GameUtils.IsContains(data,str)
+    return CS.API.IsContains(data,str)
 end
 
 --设置屏幕转向【true竖屏】【false横屏】
 function Utils:setScreen(isPortrait)
-    CS.GameUtils.SetScreen(isPortrait)
+    CS.API.SetScreen(isPortrait)
 end
 
 
@@ -74,21 +74,6 @@ end
 --创建Player角色
 function Utils:createPlayer()
     ModelManager:CreateModel("Player",false)
-end
-
---发送消息到服务器
-function Utils:sendToServer(type,url,key,json,callback)
-    Http.Instance:Message(type,url,key,json,callback)
-end
-
---发送消息到Token服务器
-function Utils:sendToToken(url,userId,userToken,key,json,callback)
-    Http.Instance:MessageToken(url,userId,userToken,key,json,callback)
-end
-
---发送消息到WWWForm服务器
-function Utils:sendToForm(url,wwwform,callback)
-    Http.Instance:MessageForm(url,wwwform,callback)
 end
 
 
